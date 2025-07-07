014892465b13fefd317f553cfc9d0fe0f38545c7
Shreel23 <shreelj59@.com>
2025-07-07 13:10:56 +0530

    frontend

--- server/routes/demo.js
+++
@@ -1,8 +0,0 @@
-import { DemoResponse } from "../../shared/api.js";
-
-export const handleDemo = (req, res) => {
-  const response = {
-    message: "Hello from Express server",
-  };
-  res.status(200).json(response);
-};
